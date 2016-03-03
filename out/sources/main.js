e196e19ce92e7056f242942eca8aee64def722a9
Vladimir37 <vladimir_zapas@mail.ru>
2016-03-03 21:30:22 +0000

    Content 1.0

--- out/sources/main.js
+++ out/sources/main.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // active page
     var addr = location.pathname;
     document.querySelector('a[href="' + addr + '"]').parentNode.classList.add('active');
+    // page height
+    var w = window;
+    var d = document;
+    var e = d.documentElement;
+    var g = d.getElementsByTagName('body')[0];
+    var height = w.innerHeight|| e.clientHeight|| g.clientHeight;
+    document.getElementsByClassName('wrapper_content')[0].style.minHeight = (height - 148) + 'px';
 });
