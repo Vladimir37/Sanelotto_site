414b728538065111fd3fd7df24598e73b42d2942
Vladimir37 <vladimir_zapas@mail.ru>
2016-03-03 03:33:28 +0000

    Design 0.2

--- src/render/sources/main.js
+++ src/render/sources/main.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
     var addr = location.pathname;
-    document.querySelector('a[href="' + addr + '"] nav').classList.add('active_nav');
+    document.querySelector('a[href="' + addr + '"]').parentNode.classList.add('active');
 });
